0613c7c36ef1c702c74a8e39bc23120c638446ed
Michael Bonato <michael.bonato@gmail.com>
2025-11-25 15:23:09 +0100

    fix bouton small mobile

--- src/features/layout/header.jsx
+++ src/features/layout/header.jsx
@@ -1,6 +1,6 @@
 export default function header({ onOpenModal }) {
   return (
-    <header className="header flex s-row jc-around ai-center w-100">
+    <header className="header flex s-row jc-between ai-center w-100">
       <img src="clear-day.svg" alt="ajouter" />
       <span>Weaver Dashbord</span>
       <button className="flex jc-center ai-center" type="button" onClick={onOpenModal} aria-label="Ajouter une localisation">+</button>
