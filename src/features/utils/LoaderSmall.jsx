91ca3a846dee963951a7f9c068ca4b7ed807d71a
Michael Bonato <michael.bonato@gmail.com>
2025-12-10 15:54:18 +0100

    image loader aléatoire

--- src/features/utils/LoaderSmall.jsx
+++ src/features/utils/LoaderSmall.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react';
+
+const loaderImages = [
+    "clear-day.svg",
+    "clear-night.svg",
+    "drizzle.svg",
+    "hail.svg",
+    "mist.svg",
+    "overcast.svg",
+    "rain.svg",
+    "snow.svg",
+    "thermometer.svg",
+    "thunderstorms.svg",
+    "uv-index.svg",
+    "windsock.svg",
+];
 
 export const LoaderSmallScreen = () => {
+    const [randomImage] = useState(() => {
+      const index = Math.floor(Math.random() * loaderImages.length);
+      return loaderImages[index];
+    });
+
     return (
         <div className="weatherItem flex w-100 m-w-50 l-w-33">
             <div className="fadeInFromLeft w-100 flex jc-center ai-center">
-                <img src="wind-toy.svg" alt="loading"/>
+                <img src={randomImage} alt="loading" />
             </div>
         </div>
-        
-    )
-}
+    );
+};
